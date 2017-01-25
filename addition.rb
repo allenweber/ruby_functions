24be7c3c6f3a9c89963bc87a9d224617ec7320a8
allenweber <weberallen1@gmail.com>
2017-01-25 14:22:39 -0800

    strings and arrays

--- addition.rb
+++ addition.rb
@@ -2,10 +2,31 @@ def add(num_1,num_2)
 	num_1 + num_2
 end
 
-def multiply(num_3,num_4)
-	num_3 * num_4
+def three(num_0, num_1, *nums)
+	result = 0
+	nums.each do |n|
+		result += nums[n - 1]
+	end
+
+	if num_0.class == String && num_1.class == String || (num_0.class == String && num_1.class == String && nums.all? {|n| n.class == String})
+		num_0 + num_1
+
+	end	
+	print nums
+	result = result + num_1 + num_0
 end
+
+# def burger(array,selection)
+# 	array + selection
+# end
+
+# array = ["one", "two", "three"]
+# selection = ["four", "five", "hamburger"]
 
 # def add(num_1,num_2)
 # 	5
+# end
+
+# def fruit(word_1,word_2)
+# 	word_1 + word_2
 # end
--- addition_test.rb
+++ addition_test.rb
@@ -1,6 +1,9 @@
 require "minitest/autorun"
 require_relative "addition.rb"
 
+# array = ["one", "two", "three"]
+# selection = ["four", "five", "hamburger"]
+
 class TestAddition < Minitest::Test
 
 	def test_1_returns_2
@@ -10,8 +13,13 @@ class TestAddition < Minitest::Test
 	def test_2_returns_5
 		assert_equal(5, add(3,2))
 	end
-
-	def test_3_return_14
-		assert_equal(14, multiply(2,7))
+	# def test_3_return
+	# 	assert_equal("'one', 'two', 'three', 'four', 'five', 'hamburger'", burger(array,selection))		
+	# end
+	# splat
+	def test_4_pass_three_arguments
+		assert_equal(3, three(1,1,1))
 	end
+
+
 end
