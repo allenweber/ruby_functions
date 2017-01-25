21d2ec4b94ea1e7352cfc0d1a951daeaeba5b430
allenweber <weberallen1@gmail.com>
2017-01-25 11:46:55 -0800

    3 test passes

--- addition.rb
+++ addition.rb
@@ -1,3 +1,11 @@
 def add(num_1,num_2)
-	2
+	num_1 + num_2
 end
+
+def multiply(num_3,num_4)
+	num_3 * num_4
+end
+
+# def add(num_1,num_2)
+# 	5
+# end
--- addition_test.rb
+++ addition_test.rb
@@ -6,4 +6,12 @@ class TestAddition < Minitest::Test
 	def test_1_returns_2
 		assert_equal(2, add(1,1))
 	end
+
+	def test_2_returns_5
+		assert_equal(5, add(3,2))
+	end
+
+	def test_3_return_14
+		assert_equal(14, multiply(2,7))
+	end
 end
